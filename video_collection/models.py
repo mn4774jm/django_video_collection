132dfd043bc839cdb42f4800bc2d1baacc400d92
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-11-17 15:21:51 -0600

    app running as expected

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from urllib import parse
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        # checks for valid youtube url in the form
+        # https://www.youtube.com/watch?v=12345678
+        # where 12345678 is the video id
+        # extract the video id from the url, prevent save if not valid youtube url or id is not found in url
+        try:
+            url_components = parse.urlparse(self.url)
+            if url_components.scheme != 'https' or url_components.netloc != 'www.youtube.com' or url_components.path != '/watch':
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+
+            query_string = url_components.query
+            if not query_string:
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            parameters = parse.parse_qs(query_string, strict_parsing=True)
+            parameter_list = parameters.get('v')
+            if not parameter_list:  # empty string, empty list...
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            self.video_id = parameter_list[0]   # set the video ID for this Video object
+        except ValueError as e: # URL parsing errors, malformed URLs
+            raise ValidationError(f'Unable to parse URL {self.url}') from e
+
+        super().save(*args, **kwargs)   # Don't forget
+
+    def __str__(self):
+        # String displayed in the admin console, or when printing a model object.
+        # You can return any useful string here. Suggest truncating notes to max 200 characters.
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, \
+        Video ID: {self.video_id}, Notes: {self.notes[:200]}'
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm, SearchForm
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.db.models.functions import Lower
 
+
+def home(request):
+    app_name = 'Exercise Videos'
+    return render(request, 'video_collection/home.html', {'app_name':app_name})
 # Create your views here.
+
+
+def add(request):
+    if request.method == 'POST':    # adding new video
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            try:
+                new_video_form.save()   # creates new Video object and saves
+                return redirect('video_list')
+            # todo redirect to list of videos
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        messages.warning(request, 'Check the data entered')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+
+def video_list(request):
+
+    search_form = SearchForm(request.GET)
+
+    if search_form.is_valid():
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name'))
+
+    else:
+        search_form = SearchForm()
+        videos = Video.objects.order_by(Lower('name'))
+
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
+
